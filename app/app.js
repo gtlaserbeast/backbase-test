cdd2b398266d93557a9773d58b2698597a21e3d3
Will Fisher <willcfisherjr@gmail.com>
2017-05-28 17:45:51 -0400

    init preview view

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('backbaseApp', [
-  'ngRoute',
-  'backbaseApp.view1'
+	'ngRoute',
+	'backbaseApp.view1'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/view1'});
+	$locationProvider.hashPrefix('!');
+	$routeProvider.otherwise({redirectTo: '/view1'});
 }]);
